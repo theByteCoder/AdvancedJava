5aaf70d199f0ed627da09e193505953479eaeb12
theByteCoder <subhasish064@gmail.com>
2022-01-17 13:50:24 +0530

    feat(core): Updated optionals

--- src/main/java/com/optionals/_Optional.java
+++ src/main/java/com/optionals/_Optional.java
@@ -28,12 +28,31 @@ public class _Optional {
                         () -> System.out.println("Null")
                 );
 
+        String name = "Subhasish";
+        Optional<String> nameOptional = Optional.ofNullable("Subhasish");
+        System.out.println(name == nameOptional.get());
+        System.out.println(nameOptional.isEmpty());
+        System.out.println(nameOptional.isPresent());
+
+        // empty
+        Optional<String> nameOptionalEmpty = Optional.empty();
+        System.out.println(nameOptionalEmpty.isEmpty());
+        System.out.println(nameOptionalEmpty.isPresent());
+
+        // empty or-else
+        String nameOptionalEmptyOrElse = nameOptionalEmpty.orElse("Default value");
+        System.out.println(nameOptionalEmptyOrElse);
+
+        // empty or-else-get
+        String emptyOrElseGet = (String) Optional.ofNullable(null).orElseGet(() -> "Default or else get");
+        System.out.println(emptyOrElseGet);
+
+        // exception
         Optional<Object> exceptionOptional = Optional
                 .ofNullable(null);
         final Supplier<IllegalStateException> illegalStateExceptionSupplier = () -> new IllegalStateException("Illegal State");
         Object exception = exceptionOptional
                 .orElseThrow(illegalStateExceptionSupplier);
         System.out.println(exception);
-
     }
 }
